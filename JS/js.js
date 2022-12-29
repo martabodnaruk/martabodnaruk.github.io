df2cdd891f62ee12fc36b573e120372bd53f5aee
martabodnaruk <117110319+martabodnaruk@users.noreply.github.com>
2022-12-29 16:15:45 +0200

    Update js.js

--- JS/js.js
+++ JS/js.js
@@ -18,7 +18,7 @@ function paginator(items, page, per_page) {
   var page = page || 1,
     per_page = per_page || 6,
     offset = (page - 1) * per_page,
-    paginatedItems = items.slice(offset).slice(0, per_page),
+    paginatedItems = items.slice(offset).slice(0, per_page);
   return {
     data: paginatedItems,
   };
